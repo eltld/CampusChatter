fa04c61ca0853986dacafadbfa5ccdd97804adf0
Yi <yigu@andrew.cmu.edu>
2013-11-17 23:37:35 -0500

    post with parse

--- src/com/campuschatter/PostActivity.java
+++ src/com/campuschatter/PostActivity.java
@@ -1,16 +1,19 @@
 package com.campuschatter;
 
 import android.app.Activity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.campuschatter.R;
-
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class PostActivity extends Activity {
+	private EditText description;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,20 @@ public class PostActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.post, menu);
 		return true;
+	}
+	
+	// post the status to the database
+	public void postSth(View button){
+		description = (EditText) findViewById(R.id.story_description);
+		String content = description.getText().toString();
+		ParseUser me = ParseUser.getCurrentUser();
+		ParseObject status = new ParseObject("status");
+		status.put("user", me);
+		status.put("status", content);
+		
+		status.saveInBackground();
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
 	}
 
 }
