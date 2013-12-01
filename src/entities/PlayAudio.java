923c3a45c717321266b7bca7ea15a7bba4c318c2
Gautham Nagaraju <gnagaraj@andrew.cmu.edu>
2013-12-01 16:58:00 -0500

    Changed the way Byte array video works. Added byte array audio playing. Changed the location of screenshot saving as there is no sd card in our tablet.

--- src/entities/PlayAudio.java
+++ src/entities/PlayAudio.java
@@ -1,8 +1,11 @@
 package entities;
 
+import android.content.Context;
+
 public interface PlayAudio {
 	public void createPlayer();
 	public void playAudio(String uri);
+	public void playAudio(Context context, byte[] audio, String type);
 	public void stopAudio();
 	public void pauseAudio();
 }
