f655a32d8df6ca6b9c8404260dd62f9f33d194f1
Jessica Lo <jlo1@andrew.cmu.edu>
2013-12-03 04:42:14 -0500

    audio plays now yayyy

--- src/activities/ViewStoryActivity.java
+++ src/activities/ViewStoryActivity.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import android.widget.VideoView;
 
 import com.example.campuschatter.R;
 
+import entities.PlayAudioAPI;
 import entities.PlayVideo;
 import entities.PlayVideoAPI;
 
@@ -26,7 +29,7 @@ public class ViewStoryActivity extends Activity {
 		String title = (String) getIntent().getExtras().get("title");
 		String author = (String) getIntent().getExtras().get("author");
 		String description = (String) getIntent().getExtras().get("description");
-		byte[] media = (byte[]) getIntent().getExtras().get("media");
+		final byte[] media = (byte[]) getIntent().getExtras().get("media");
 		int mediaType = (Integer) getIntent().getExtras().get("mediaType");
 
 		((TextView)findViewById(R.id.story_title_view)).setText(title);
@@ -49,6 +52,17 @@ public class ViewStoryActivity extends Activity {
 			pv.playVideo(media, vv, this);
 			fl.addView(vv);
 		} else if (mediaType == Story.AUDIO_TYPE) {
+			FrameLayout afl = (FrameLayout)LayoutInflater.from(
+					getApplicationContext()).inflate(R.layout.audio_story, null);
+			final ImageView ivPlay = (ImageView)afl.findViewById(R.id.play_audio);
+			final PlayAudioAPI api = new PlayAudioAPI();
+			ivPlay.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View arg0) {
+					api.playAudio(getApplicationContext(), media);	
+				}
+			});
+			fl.addView(afl);
 			
 		}
 
--- src/entities/PlayAudio.java
+++ src/entities/PlayAudio.java
@@ -5,7 +5,7 @@ import android.content.Context;
 public interface PlayAudio {
 	public void createPlayer();
 	public void playAudio(String uri);
-	public void playAudio(Context context, byte[] audio, String type);
+	public void playAudio(Context context, byte[] audio);
 	public void stopAudio();
 	public void pauseAudio();
 }
